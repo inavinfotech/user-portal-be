6137f3a360728fab00f9b649c8462eb46dde2d78
DeveloperSwastik <developerswastiksharma@gmail.com>
2026-04-14 11:11:35 +0530

    chore: update uvicorn server port from 8000 to 8001

--- app/main.py
+++ app/main.py
@@ -39,4 +39,4 @@ app.include_router(users.router, prefix=f"{settings.API_V1_STR}/users", tags=["u
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
