93589498da571357242428b43fe8796cc12d8bee
DeveloperSwastik <developerswastiksharma@gmail.com>
2026-07-22 21:47:42 +0530

    feat: update ALLOWED_ORIGINS to allow all origins via wildcard

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,8 @@ from typing import Optional
 class Settings(BaseSettings):
     PROJECT_NAME: str = "User Management System"
     API_V1_STR: str = "/api/v1"
-    ALLOWED_ORIGINS: list[str] = ["http://localhost:5173", "http://localhost:8000", "http://127.0.0.1:5173", "http://127.0.0.1:8000"]
+    ALLOWED_ORIGINS: list[str] = ["*"]
+
     
     # Database
     DATABASE_URL: str = "sqlite:///./sql_app.db"
